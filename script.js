bde64ebf5ccffa26d61a6e627dfd0cdabb17d86b
farhantaskinismail1 <ismailaarr8@gmail.com>
2025-09-13 23:07:02 +0600

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const btn = document.getElementById('no_b');
 
 btn.addEventListener('mouseenter', () => {
-  // x যেকোনো random (0 - 300px)
   const x = Math.floor(Math.random() * 300);
 
-  // y সবসময় উপরের দিকে (negative value)
-  const y = -Math.floor(Math.random() * 200); // উপরের দিকে max 200px যাবে
+  const y = -Math.floor(Math.random() * 200);
+  btn.style.position = 'relative';
+  btn.style.transition = 'transform 0.3s ease';
+  btn.style.transform = `translate(${x}px, ${y}px)`;
+});
+/////////////////////////////////////////
+btn.addEventListener('click', () => {
+  const x = Math.floor(Math.random() * 400);
+  const y = -Math.floor(Math.random() * 300);
 
   btn.style.position = 'relative';
   btn.style.transition = 'transform 0.3s ease';
